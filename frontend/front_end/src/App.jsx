ee19bc2e3985304789701566d1a029dd34b09687
Naing Win <uunaing111@gmail.com>
2026-01-11 00:09:54 +0630

    added login routes

--- frontend/front_end/src/App.jsx
+++ frontend/front_end/src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import UserHome from "./pages/UserHome";
 
 function App() {
-  const fetchMessage = () => {
-    fetch("http://localhost:8000/products") // simple GET
-      .then(res => res.json())
-      .then(data => alert(data.message))
-      .catch(err => console.error(err));
-  };
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Laravel ↔ React Test</h1>
-      <button onClick={fetchMessage}>Fetch Message</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/user/home" element={<UserHome />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
---
+++ frontend/front_end/src/pages/UserHome.jsx
@@ -0,0 +1,10 @@
+function UserHome() {
+  return (
+    <div>
+      <h1>Customer Home Page</h1>
+      <p>Welcome, you are logged in as customer.</p>
+    </div>
+  );
+}
+
+export default UserHome;
