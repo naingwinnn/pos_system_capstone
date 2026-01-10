efa5d2835ae3847e275bcd6c29cd2b04a53e6093
Naing Win <uunaing111@gmail.com>
2026-01-10 22:23:00 +0630

    modified relationships1

--- backend/laravel-backend/app/Models/Review.php
+++ backend/laravel-backend/app/Models/Review.php
@@ -6,13 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    // A review belongs to a user
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // A review belongs to a shop
     public function shop()
     {
         return $this->belongsTo(Shop::class);
